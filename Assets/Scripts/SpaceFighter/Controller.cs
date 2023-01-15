11f187c2efd4c37ffa693496b5960f693236f235
Alexey <alexeyagaphonov@gmail.com>
2023-01-15 16:48:45 +0200

    Basic smooth movement

---
+++ Assets/Scripts/SpaceFighter/Controller.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+namespace SpaceFighter
+{
+    public class Controller : MonoBehaviour
+    {
+        [SerializeField] private GameObject _pawnGameObject;
+        private Rigidbody2D _pawnRigidbody;
+
+        [SerializeField] private float _maxSpeed = 5.0f;
+        [SerializeField] private float _smoothTime = 0.1f;
+
+        private Vector2 _targetPosition;
+        private Vector2 _currentVelocity;
+
+
+        private void Awake()
+        {
+            if (_pawnGameObject == null)
+            {
+                _pawnGameObject = transform.parent.gameObject;
+            }
+            _pawnRigidbody = _pawnGameObject.GetComponent<Rigidbody2D>();
+            _targetPosition = _pawnRigidbody.position;
+        }
+
+        private void Update()
+        {
+            if(Input.GetMouseButton(0))
+            {
+                _targetPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            }
+
+            _pawnGameObject.transform.position = Vector2.SmoothDamp(_pawnGameObject.transform.position, _targetPosition, ref _currentVelocity, _smoothTime, _maxSpeed, Time.deltaTime);
+        }
+    }
+
+}
